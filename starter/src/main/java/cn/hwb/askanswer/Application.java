096ed869efab0abfa81dc6bfe60d845774c3bd50
Etherealss <1831669777@qq.com>
2023-03-24 00:42:06 +0800

    feat(starter): MyBatisPlus配置

--- starter/src/main/java/cn/hwb/askanswer/Application.java
+++ starter/src/main/java/cn/hwb/askanswer/Application.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @date 2023-03-21
  */
 // 指定要扫描的Mapper类的包的路径，需要加上Mapper注解，否则会把访问到的接口都当成Mapper类
-    @MapperScan(value = "cn.hwb", annotationClass = Mapper.class)
+@MapperScan(value = "cn.hwb", annotationClass = Mapper.class)
 @ComponentScan("cn.hwb") // 扫描其他子模块的组件
 @SpringBootApplication()
 public class Application {
