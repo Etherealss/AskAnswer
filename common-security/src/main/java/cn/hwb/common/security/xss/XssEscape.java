f73ab87f3bd304a5da5419a749e96d752076d0a9
Etherealss <1831669777@qq.com>
2023-03-23 12:36:30 +0800

    feat(security): XSS转义功能

---
+++ common-security/src/main/java/cn/hwb/common/security/xss/XssEscape.java
@@ -0,0 +1,14 @@
+package cn.hwb.common.security.xss;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * xss 转义
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface XssEscape {
+}
---
+++ common-security/src/main/java/cn/hwb/common/security/xss/XssEscapeAspect.java
@@ -0,0 +1,41 @@
+package cn.hwb.common.security.xss;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.StringEscapeUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author wtk
+ * @date 2023-03-23
+ */
+@Aspect
+@Component
+@Slf4j
+public class XssEscapeAspect {
+
+    @Before("@annotation(cn.hwb.common.security.xss.XssEscape)")
+    public void escape(JoinPoint joinPoint) throws Exception {
+        log.info("进行XSS转义");
+        for (Object arg : joinPoint.getArgs()) {
+            if (arg != null && arg.getClass().isAnnotationPresent(XssEscape.class)) {
+                Field[] fields = arg.getClass().getDeclaredFields();
+                for (Field field : fields) {
+                    if (field.isAnnotationPresent(XssEscape.class) && field.getType().equals(String.class)) {
+                        field.setAccessible(true);
+                        String value = (String) field.get(arg);
+                        if (value != null) {
+                            String escapedValue = StringEscapeUtils.escapeHtml4(value);
+                            escapedValue = StringEscapeUtils.escapeEcmaScript(escapedValue);
+                            field.set(arg, escapedValue);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
