d9317e5587def253c25d0772f7e37c28db00119a
Etherealss <1831669777@qq.com>
2023-04-06 00:40:05 +0800

    feature(notification): 通知功能

---
+++ modules-notification/src/main/java/cn/hwb/askanswer/notification/infrastructure/entity/NotificationEntity.java
@@ -0,0 +1,21 @@
+package cn.hwb.askanswer.notification.infrastructure.entity;
+
+import cn.hwb.askanswer.common.base.pojo.entity.IdentifiedEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author wtk
+ * @date 2023-04-06
+ */
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName(value = "notification")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class NotificationEntity extends IdentifiedEntity {
+}
---
+++ modules-notification/src/main/java/cn/hwb/askanswer/notification/service/NotificationService.java
@@ -0,0 +1,15 @@
+package cn.hwb.askanswer.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wtk
+ * @date 2023-04-06
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+}
