3c4ae2c9bd556391f976578574e82082c4f773c5
Etherealss <1831669777@qq.com>
2023-04-14 20:58:46 +0800

    bugfix: AnswerDTO questionId缺失

--- modules-answer/src/main/java/cn/hwb/askanswer/answer/infrastructure/pojo/dto/AnswerDTO.java
+++ modules-answer/src/main/java/cn/hwb/askanswer/answer/infrastructure/pojo/dto/AnswerDTO.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AnswerDTO {
     Long id;
+    Long questionId;
     UserBriefDTO creator;
     Date createTime;
     Date modifyTime;
